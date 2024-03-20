a85ad3d40e877efff8cdc6fe28d3ba48a471a92a
Yannick-btn <143724027+Yannick-btn@users.noreply.github.com>
2024-03-20 00:51:48 -0400

    sprite ajout
    ajout d'un sprite lorsque le joueur sapproche du npc

--- Assets/Scripts/Joueur/PlayerInteract.cs
+++ Assets/Scripts/Joueur/PlayerInteract.cs
@@ -19,7 +19,30 @@ public class PlayerInteract : MonoBehaviour
                     // Appeler la méthode Interact de l'objet SwitchInteract
                     switchInteractable.Interact();
                 }
+
+                if (collider.TryGetComponent(out NPCInteraction npcInteraction)) {
+                    // Appeler la méthode Interact de l'objet SwitchInteract
+                    npcInteraction.Interact();
+                }
             }
         }
+    }
+    public NPCInteraction GetInterationObject() {
+     
+            // distance des collider avec lequel le jouer peut interagir
+            float interactRange = 2f;
+
+            // interraction avec tout les collider autour du joueur
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+
+            // cherche tout les collider interagis
+            foreach (Collider collider in colliderArray) {
+                // Vérifier si le collider possède un composant SwitchInteract
+                if (collider.TryGetComponent(out NPCInteraction npcInteraction)) {
+                    return npcInteraction;
+                }
+            }
+        
+        return null;
     }
 }
