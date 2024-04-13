c74320c7be6415d2d9346e767436929a6072bd39
Yannick-btn <143724027+Yannick-btn@users.noreply.github.com>
2024-04-13 19:59:52 -0400

    ajout des animations clapet et modification du code

--- Assets/Scripts/Boutons/SwitchInteract.cs
+++ Assets/Scripts/Boutons/SwitchInteract.cs
@@ -3,35 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchInteract : MonoBehaviour {
-    // Référence à la boîte à animer
-    [SerializeField] private GameObject boxToAnimate;
-    [SerializeField] private GameObject boxToAnimate2;
+    // Liste des boîtes à animer
+    [SerializeField] private List<GameObject> boxesToAnimate = new List<GameObject>();
 
-    // Référence à l'Animator de la boîte
-    private Animator boxAnimator;
+    // Liste des Animator des boîtes
+    private List<Animator> boxAnimators = new List<Animator>();
 
-    // Variable pour suivre l'état de déplacement de la boîte
+    // Variable pour suivre l'état de déplacement des boîtes
     private bool moved;
 
     // Constante pour le nom du paramètre de l'Animator
     private const string HAS_MOVED = "HasMoved";
 
     // Méthode appelée au démarrage de l'objet
-    private void Start() {
-        // Récupération de l'Animator attaché à la boîte
-        boxAnimator = boxToAnimate.GetComponent<Animator>();
-        boxAnimator = boxToAnimate2.GetComponent<Animator>();
+    private void Awake() {
+        // Récupération des Animator attachés aux boîtes
+        foreach (GameObject box in boxesToAnimate) {
+            Animator animator = box.GetComponent<Animator>();
+            if (animator != null) {
+                boxAnimators.Add(animator);
+            }
+        }
     }
 
-    // Méthode publique appelée lors de l'interaction avec la boîte
+    // Méthode publique appelée lors de l'interaction avec les boîtes
     public void Interact() {
         // Inversion de l'état de déplacement
         moved = !moved;
-        // Vérification si l'Animator existe
-        if (boxAnimator != null) {
-            // Mise à jour du paramètre booléen en fonction de l'état actuel
-            boxAnimator.SetBool(HAS_MOVED, moved);
+
+        // Mise à jour de l'état de toutes les boîtes
+        foreach (Animator animator in boxAnimators) {
+            if (animator != null) {
+                animator.SetBool(HAS_MOVED, moved);
+            }
         }
     }
 }
-
---
+++ Assets/Scripts/Boutons/SwitchInteractTemps.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchInteractTemps : MonoBehaviour {
+    // Liste de boîtes à animer
+    [SerializeField] private List<GameObject> boxesToAnimate = new List<GameObject>();
+
+    // Liste d'Animator de boîtes
+    private List<Animator> boxAnimators = new List<Animator>();
+
+    // Variable pour suivre l'état de déplacement des boîtes
+    private bool moved;
+
+    // Constante pour le nom du paramètre de l'Animator
+    private const string HAS_MOVED = "HasMoved";
+
+    // Nombre de secondes avant d'inverser l'état moved
+    [SerializeField] private float inversionDelay = 10f;
+
+    // Temps pendant lequel le joueur ne peut pas interagir après une interaction
+    [SerializeField] private float interactionCooldownTime = 8f;
+
+    // Variable pour suivre l'état de l'interaction
+    private bool interactionEnabled = true;
+
+    // Méthode appelée au démarrage de l'objet
+    private void Awake() {
+        // Récupération des Animator attachés aux boîtes
+        foreach (GameObject box in boxesToAnimate) {
+            Animator animator = box.GetComponent<Animator>();
+            if (animator != null) {
+                boxAnimators.Add(animator);
+            }
+        }
+    }
+
+    // Méthode publique appelée lors de l'interaction avec les boîtes
+    public void Interact() {
+        if (!interactionEnabled) {
+            return; // Si l'interaction est désactivée, ne pas permettre l'interaction
+        }
+
+        // Inversion de l'état de déplacement
+        moved = !moved;
+
+        // Mise à jour de l'état de toutes les boîtes
+        foreach (Animator animator in boxAnimators) {
+            if (animator != null) {
+                animator.SetBool(HAS_MOVED, moved);
+            }
+        }
+
+        // Désactiver l'interaction pendant le délai d'inversion
+        interactionEnabled = false;
+
+        // Lancer la coroutine pour inverser moved après inversionDelay secondes
+        StartCoroutine(InverserMovedApresDelay());
+
+        // Lancer la coroutine pour activer à nouveau l'interaction après interactionCooldownTime secondes
+        StartCoroutine(EnableInteractionApresCooldown());
+    }
+
+    // Coroutine pour inverser moved après inversionDelay secondes
+    private IEnumerator InverserMovedApresDelay() {
+        // Attendre pendant inversionDelay secondes
+        yield return new WaitForSeconds(inversionDelay);
+
+        // Inverser moved après le délai
+        moved = !moved;
+
+        // Mise à jour de l'état de toutes les boîtes
+        foreach (Animator animator in boxAnimators) {
+            if (animator != null) {
+                animator.SetBool(HAS_MOVED, moved);
+            }
+        }
+    }
+
+    // Coroutine pour activer à nouveau l'interaction après interactionCooldownTime secondes
+    private IEnumerator EnableInteractionApresCooldown() {
+        // Attendre pendant interactionCooldownTime secondes
+        yield return new WaitForSeconds(interactionCooldownTime);
+
+        // Activer à nouveau l'interaction
+        interactionEnabled = true;
+    }
+}
