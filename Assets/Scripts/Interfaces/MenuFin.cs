b9196e69cfd5edb7966b59cbd590ba2a6f1c4b0e
MaggieD20 <coucoumaggie@hotmail.com>
2024-04-17 15:55:39 -0400

    logo

--- Assets/Scripts/Interfaces/MenuFin.cs
+++ Assets/Scripts/Interfaces/MenuFin.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class MenuFin : MonoBehaviour
 {
+    //variable prenant le parant des deux boutons
+    public GameObject boutons;
+    
+    //Fonction qui retourne au menu du début
     public void RetourMenu()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
+    //fonction pour arrêterle jeu
     public void Quitter()
     {
         Application.Quit();
+    }
+
+    //fonction pour faire apparaitre les boutons
+    public void BoutonsVisibles()
+    {
+        boutons.SetActive(true);
     }
 }
