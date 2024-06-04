94275dec7a9acca0ea85698cb4bebefc45fedf23
Yannick-btn <143724027+Yannick-btn@users.noreply.github.com>
2024-06-03 20:31:17 -0400

    build

--- Assets/Scripts/Interfaces/TransitionsMenus.cs
+++ Assets/Scripts/Interfaces/TransitionsMenus.cs
@@ -1,60 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
-// Logique du menu
-
-public class TransitionsMenus : MonoBehaviour
-{
-    // Variables statiques pour stocker les etats des menus
+public class TransitionsMenus : MonoBehaviour {
+    // Variables statiques pour stocker les états des menus
     static GameObject MenuActif;
     static GameObject NouveauMenu;
     static GameObject MenuPrecedent;
 
-    // gameobjects pour les menus et segments d'interface
+    // GameObjects pour les menus et segments d'interface
     public GameObject UIComplet;
     public GameObject MenuPrincipal;
     public GameObject pauseMenu;
     public GameObject UIJeu;
     public GameObject transition;
 
-    // Variable pour stocker l'etat du jeu
+    // Variable pour stocker l'état du jeu
     public static bool enPause;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
-        {
+
+    // Start est appelée avant la première mise à jour de la frame
+    void Start() {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0) {
             MenuActif = MenuPrincipal;
-        } 
-        else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 1 || UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 2 || UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 3)
-        {
+        } else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 1 || UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 2 || UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 3) {
             MenuActif = GameObject.Find("MenuPause");
         }
-        
     }
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 0 && UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 5)
-        {
-            if (enPause)
-            {
+
+    // Update est appelée une fois par frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) && UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 0 && UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 5) {
+            if (enPause) {
                 ResumeJeu();
-            } else
-            {
+            } else {
                 PauseJeu();
             }
-        }   
+        }
     }
-    
-    // Permet de r�sumer le jeu
-    public void ResumeJeu()
-    {
+
+    // Permet de reprendre le jeu
+    public void ResumeJeu() {
         DeterminerNouveauMenu(UIJeu);
         TransitionMenu();
         pauseMenu.SetActive(false);
@@ -64,9 +50,8 @@ public class TransitionsMenus : MonoBehaviour
         enPause = false;
     }
 
-    // Permet de mettre en pause
-    public void PauseJeu()
-    {
+    // Permet de mettre le jeu en pause
+    public void PauseJeu() {
         DeterminerNouveauMenu(pauseMenu);
         TransitionMenu();
         Cursor.lockState = CursorLockMode.None; // Déverrouille le curseur.
@@ -75,63 +60,49 @@ public class TransitionsMenus : MonoBehaviour
         enPause = true;
     }
 
-    // change la scene active
-    public void ChangementScene(int Scene)
-    {
+    // Change la scène active
+    public void ChangementScene(int Scene) {
         Time.timeScale = 1f;
-        if (Scene > 0 && Scene < 5)
-        {
+        if (Scene > 0 && Scene < 5) {
             NouveauMenu = UIJeu;
-        }
-        else if (Scene == 0 || Scene == 5)
-        {
+        } else if (Scene == 0 || Scene == 5) {
             NouveauMenu = MenuPrincipal;
             Destroy(UIComplet);
         }
         TransitionMenu();
-        // if(Scene == 5)
-        // {
-        //     Destroy(UIComplet);
-        // }
         UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
         enPause = false;
     }
 
-    // Determine le menu a activer
-    public void DeterminerNouveauMenu(GameObject CibleMenu)
-    {
+    // Détermine le menu à activer
+    public void DeterminerNouveauMenu(GameObject CibleMenu) {
         NouveauMenu = CibleMenu;
     }
 
     // Transition entre les menus
-    public void TransitionMenu()
-    {   
-        if (MenuActif != null)
-        {
+    public void TransitionMenu() {
+        if (MenuActif != null) {
             MenuActif.SetActive(false);
         }
-        if (NouveauMenu != null)
-        {
+        if (NouveauMenu != null) {
             NouveauMenu.SetActive(true);
             MenuPrecedent = MenuActif;
             MenuActif = NouveauMenu;
         }
     }
-    // Retour au menu precedent
-    public void RetourMenuPrecedent()
-    {
+
+    // Retourne au menu précédent
+    public void RetourMenuPrecedent() {
         DeterminerNouveauMenu(MenuPrecedent);
     }
 
-    // Desactive la transition apres qu'elle soit terminee
-    public void DesactiverTransition()
-    {
+    // Désactive la transition après qu'elle soit terminée
+    public void DesactiverTransition() {
         transition.SetActive(false);
-    }      
+    }
 
-    // Quitter le jeu au complet
-        public void QuitJeu()
-    {
+    // Quitte le jeu complètement
+    public void QuitJeu() {
         Application.Quit();
     }
 }
